0e42cba6ee6d53130156372f9529de6f61c3cfbb
tomekun <154856233+tomekun@users.noreply.github.com>
2024-01-20 11:06:25 +0900

    test

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log("test")
+const http = require("http");
+const axios = require('axios');
+const fs = require('node:fs')
+function keepServer(){
+try {
+http.createServer(function(req, res) {
+  res.write("OK！");
+  res.end();
+}).listen(8080);
+} catch(e){console.log('エラーが発生しました。\nエラー内容:'+e)}
+}
+exports.server = keepServer;
+
+
+
